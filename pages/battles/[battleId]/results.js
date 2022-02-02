1469ba457f0509eef9c60c4baec1905195aa1e43
Nico Andrade <nicoandrade@gmail.com>
2022-02-02 20:44:47 -0300

    update

--- pages/battles/[battleId]/results.js
+++ pages/battles/[battleId]/results.js
@@ -125,6 +125,7 @@ export default function BattleResults({ battle }) {
                         },
                     ],
                     site_name: "WarWordly",
+                    url: battleURL,
                 }}
             />
 
