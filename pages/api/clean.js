18d8acc2671dd72653a2b337fa4cf5532affac41
Nico Andrade <nicoandrade@gmail.com>
2022-02-03 17:23:03 -0300

    better spanish words

---
+++ pages/api/clean.js
@@ -0,0 +1,29 @@
+export default async function handler(req, res) {
+    switch (req.method) {
+        case "POST":
+            return res.status(200).end(`Nothing to do`);
+
+            let { words } = await import(`constants/test_words.js`);
+
+            words = words.filter((word) => word.length == 5);
+
+            words = words.map((word) =>
+                word
+                    .normalize("NFD")
+                    .replace(
+                        /([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi,
+                        "$1"
+                    )
+                    .normalize()
+            );
+
+            return res.status(200).json(words);
+            break;
+
+        default:
+            res.setHeader("Allow", ["POST"]);
+
+            return res.status(405).end(`Method "${req.method}" not supported`);
+            break;
+    }
+}
